920b7a75ab5a3be45145a624de3ce8ff66be7de2
Kiyohisa <kadono.kiyohisa@pfu.fujitsu.com>
2014-01-16 00:17:07 +0900

    現在位置

--- Resources/iphone/alloy/controllers/Entry.js
+++ Resources/iphone/alloy/controllers/Entry.js
@@ -10,6 +10,20 @@ function Controller() {
                 return;
             }
             alert(e.coords);
+            Alloy.Globals.Map.createAnnotation({
+                latitude: 37.390749,
+                longitude: -122.081651,
+                title: "Appcelerator Headquarters",
+                subtitle: "Mountain View, CA",
+                pincolor: Alloy.Globals.Map.ANNOTATION_RED,
+                myid: 1
+            });
+            $.mapView.region = {
+                latitude: e.coords.latitude,
+                longitude: e.coords.longitude,
+                latitudeDelta: .05,
+                longitudeDelta: .01
+            };
         });
     }
     require("alloy/controllers/BaseController").apply(this, Array.prototype.slice.call(arguments));
@@ -28,27 +42,13 @@ function Controller() {
     showCurrentPosition ? $.__views.map.addEventListener("open", showCurrentPosition) : __defers["$.__views.map!open!showCurrentPosition"] = true;
     $.__views.mapView = Alloy.Globals.Map.createView({
         id: "mapView",
-        ns: "Alloy.Globals.Map"
+        ns: "Alloy.Globals.Map",
+        userLocation: "true"
     });
     $.__views.map.add($.__views.mapView);
     report ? $.__views.mapView.addEventListener("click", report) : __defers["$.__views.mapView!click!report"] = true;
     exports.destroy = function() {};
     _.extend($, $.__views);
-    var moutainView = Alloy.Globals.Map.createAnnotation({
-        latitude: 37.390749,
-        longitude: -122.081651,
-        title: "Appcelerator Headquarters",
-        subtitle: "Mountain View, CA",
-        pincolor: Alloy.Globals.Map.ANNOTATION_RED,
-        myid: 1
-    });
-    $.mapView.region = {
-        latitude: 33.74511,
-        longitude: -84.38993,
-        latitudeDelta: .01,
-        longitudeDelta: .01
-    };
-    $.mapView.addAnnotation(moutainView);
     $.map.open();
     __defers["$.__views.map!open!showCurrentPosition"] && $.__views.map.addEventListener("open", showCurrentPosition);
     __defers["$.__views.mapView!click!report"] && $.__views.mapView.addEventListener("click", report);
--- app/controllers/Entry.js
+++ app/controllers/Entry.js
@@ -16,24 +16,37 @@ function showCurrentPosition(){
 			return;
 		}
 		alert(e.coords);
+		var moutainView = Alloy.Globals.Map.createAnnotation({
+		    latitude:37.390749,
+		    longitude:-122.081651,
+		    title:"Appcelerator Headquarters",
+		    subtitle:'Mountain View, CA',
+		    pincolor:Alloy.Globals.Map.ANNOTATION_RED,
+		    myid:1 // Custom property to uniquely identify this annotation.
+		});
+		
+		$.mapView.region = {
+		latitude:e.coords.latitude, longitude:e.coords.longitude,
+		latitudeDelta:0.05, longitudeDelta:0.01,
+		};
+		$.mapView.addAnnotation(moutainView);
+		
 	});
 }
 
-var moutainView = Alloy.Globals.Map.createAnnotation({
-	latitude:37.390749,
-	longitude:-122.081651,
-	title:"Appcelerator Headquarters",
-	subtitle: 'Mountain View, CA',
-	pincolor:Alloy.Globals.Map.ANNOTATION_RED,
-	myid:1
-});
-
-$.mapView.region = {
-	latitude:33.74511,
-	longitude:-84.38993,
-	latitudeDelta:0.01,
-	longitudeDelta:0.01
-};
-$.mapView.addAnnotation(moutainView);
+// var moutainView = Alloy.Globals.Map.createAnnotation({
+    // latitude:37.390749,
+    // longitude:-122.081651,
+    // title:"Appcelerator Headquarters",
+    // subtitle:'Mountain View, CA',
+    // pincolor:Alloy.Globals.Map.ANNOTATION_RED,
+    // myid:1 // Custom property to uniquely identify this annotation.
+// });
+// 
+// $.mapView.region = {
+// latitude:33.74511, longitude:-84.38993,
+// latitudeDelta:0.01, longitudeDelta:0.01
+// };
+// $.mapView.addAnnotation(moutainView);
 
 $.map.open();
